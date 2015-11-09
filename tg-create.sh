7bae5902b25579d40b9ffd9c95b00e0c22b4f56d
Kyle J. McKay <mackyle@gmail.com>
2015-11-08 23:16:55 -0800

    tg-create.sh: implement new options and semantics
    By default tg-create now runs an editor on .topmsg and
    shepherds the initial commit on the new branch so it
    only contains the .topdeps and .topmsg changes.
    
    The old behavior can be restored by using --no-edit (or -n).
    
    Just the commit itself can be suppressed with --no-commit
    (but the editor will still be run on .topmsg in that case).
    
    As a bonus, if tg create is allowed to shepherd the initial
    commit then it will also pre-load a suitable commit message
    extracted from .topdeps into MERGE_MSG all ready for the next
    commit to save typing.
    
    The .topmsg template now includes a Signed-off-by: line. as well.
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- tg-create.sh
+++ tg-create.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 # TopGit - A different patch queue manager
-# (c) Petr Baudis <pasky@suse.cz>  2008
+# Copyright (C) Petr Baudis <pasky@suse.cz>  2008
+# Copyright (C) Kyle J. McKay <mackyle@gmail.com>  2015
+# All rights reserved.
 # GPLv2
 
 deps= # List of dependent branches
@@ -14,6 +16,7 @@ msgfile=
 noedit=
 nocommit=
 continue=
+topmsg=
 
 USAGE="Usage: ${tgname:-tg} [... -r remote] create [-m <msg> | -F <file>] [-n] [--no-commit] [<name> [<dep>...|-r [<rname>]] ]"
 
@@ -25,6 +28,19 @@ usage()
 		printf '%s\n' "$USAGE"
 	fi
 	exit ${1:-0}
+}
+
+is_active()
+{
+	[ -d "$git_dir/tg-create" ] || return 1
+	[ -s "$git_dir/tg-create/name" ] || return 1
+	[ -s "$git_dir/tg-create/deps" ] || return 1
+	[ -s "$git_dir/tg-create/merge" ] || return 1
+	[ -s "$git_dir/tg-create/msg" ] || return 1
+	[ -s "$git_dir/tg-create/topmsg" ] || return 1
+	[ -f "$git_dir/tg-create/nocommit" ] || return 1
+	[ -f "$git_dir/tg-create/noedit" ] || return 1
+	return 0
 }
 
 ## Parse options
@@ -71,7 +87,7 @@ while [ $# -gt 0 ]; do case "$1" in
 		;;
 	-r)
 		remote=1
-		rname="$1"; [ $# -eq 0 ] || shift;;
+		rname="$1"; [ $# -eq 0 ] || shift
 		;;
 	--)
 		shift
@@ -92,12 +108,15 @@ esac; shift; done
 [ -n "$continue" -o -n "$name" ] || { warn "no branch name given"; usage 1; }
 [ -z "$remote" -o -n "$rname" ] || rname="$name"
 [ $# -eq 0 -o -z "$remote" ] || { warn "deps not allowed with -r"; usage 1; }
+[ -z "$msg" -o -z "$msgfile" ] || die "only one -F or -m option is allowed"
+[ -z "$continue" ] || is_active || die "no tg create is currently active"
 
 ## Fast-track creating branches based on remote ones
 
 if [ -n "$rname" ]; then
 	[ -n "$name" ] || die "no branch name given"
 	! ref_exists "refs/heads/$name" || die "branch '$name' already exists"
+	! ref_exists "refs/top-bases/$name" || die "'top-bases/$name' already exists"
 	if [ -z "$base_remote" ]; then
 		die "no remote location given. Either use -r remote argument or set topgit.remote"
 	fi
@@ -107,22 +126,26 @@ if [ -n "$rname" ]; then
 		mkdir -p "$git_dir/logs/refs/top-bases/$(dirname "$name")" 2>/dev/null || :
 		{ >>"$git_dir/logs/refs/top-bases/$name" || :; } 2>/dev/null
 	fi
-	git update-ref "refs/top-bases/$name" "refs/remotes/$base_remote/top-bases/$rname"
-	git update-ref "refs/heads/$name" "refs/remotes/$base_remote/$rname"
+	msg="tgcreate: $name -r $rname"
+	git update-ref -m "$msg" "refs/top-bases/$name" "refs/remotes/$base_remote/top-bases/$rname" ""
+	git update-ref -m "$msg" "refs/heads/$name" "refs/remotes/$base_remote/$rname" ""
 	info "Topic branch $name based on $base_remote : $rname set up."
 	exit 0
 fi
-
 
 ## Auto-guess dependencies
 
 deps="$*"
 if [ -z "$deps" ]; then
-	if [ -z "$name" -a -s "$git_dir/top-name" -a -s "$git_dir/top-deps" -a -s "$git_dir/top-merge" ]; then
+	if [ -z "$name" ] && is_active; then
 		# We are setting up the base branch now; resume merge!
-		name="$(cat "$git_dir/top-name")"
-		deps="$(cat "$git_dir/top-deps")"
-		merge="$(cat "$git_dir/top-merge")"
+		name="$(cat "$git_dir/tg-create/name")"
+		deps="$(cat "$git_dir/tg-create/deps")"
+		merge="$(cat "$git_dir/tg-create/merge")"
+		msg="$(cat "$git_dir/tg-create/msg")"
+		topmsg="$(cat "$git_dir/tg-create/topmsg")"
+		nocommit="$(cat "$git_dir/tg-create/nocommit")"
+		noedit="$(cat "$git_dir/tg-create/noedit")"
 		restarted=1
 		info "Resuming $name setup..."
 	else
@@ -143,9 +166,68 @@ for d in $deps; do
 done
 ! ref_exists "refs/heads/$name"  ||
 	die "branch '$name' already exists"
+! ref_exists "refs/top-bases/$name" ||
+	die "'top-bases/$name' already exists"
 
 # Clean up any stale stuff
-rm -f "$git_dir/top-name" "$git_dir/top-deps" "$git_dir/top-merge"
+rm -rf "$git_dir/tg-create"
+
+
+# Get messages
+if [ -z "$restarted" ]; then
+	>"$git_dir/TG_EDITMSG"
+	if [ -n "$msgfile" ]; then
+		if [ "$msgfile" = "-" ]; then
+			git stripspace >"$git_dir/TG_EDITMSG"
+		else
+			git stripspace <"$msgfile" >"$git_dir/TG_EDITMSG"
+		fi
+	elif [ -n "$msg" ]; then
+		printf '%s\n' "$msg" | git stripspace >"$git_dir/TG_EDITMSG"
+	fi
+	if [ ! -s "$git_dir/TG_EDITMSG" ]; then
+		printf '%s\n' "tg create $name" | git stripspace >"$git_dir/TG_EDITMSG"
+	fi
+	msg="$(cat "$git_dir/TG_EDITMSG")"
+	rm -f "$git_dir/TG_EDITMSG"
+
+	author="$(git var GIT_AUTHOR_IDENT)"
+	author_addr="${author%> *}>"
+	{
+		echo "From: $author_addr"
+		! header="$(git config topgit.to)" || echo "To: $header"
+		! header="$(git config topgit.cc)" || echo "Cc: $header"
+		! header="$(git config topgit.bcc)" || echo "Bcc: $header"
+		! subject_prefix="$(git config topgit.subjectprefix)" || subject_prefix="$subject_prefix "
+		echo "Subject: [${subject_prefix}PATCH] $name"
+		echo
+		echo '<patch description>'
+		echo
+		echo "Signed-off-by: $author_addr"
+		[ "$(git config --bool format.signoff)" = true ] && echo "Signed-off-by: $author_addr"
+	} | git stripspace >"$git_dir/TG_EDITMSG"
+	if [ -z "$noedit" ]; then
+		cat <<EOT >>"$git_dir/TG_EDITMSG"
+
+# Please enter the patch message for the new TopGit branch $name.
+# It will be stored in the .topmsg file and used to create the
+# patch header when \`tg patch\` is run on branch $name.
+# The "Subject:" line will appear in \`tg summary\` and \`tg info\` output.
+#
+# Lines starting with '#' will be ignored, and an empty patch
+# message aborts the \`tg create\` operation entirely.
+#
+# tg create $name $deps
+EOT
+		run_editor "$git_dir/TG_EDITMSG" || \
+		die "there was a problem with the editor '$tg_editor'"
+		git stripspace -s <"$git_dir/TG_EDITMSG" >"$git_dir/TG_EDITMSG"+
+		mv -f "$git_dir/TG_EDITMSG"+ "$git_dir/TG_EDITMSG"
+		[ -s "$git_dir/TG_EDITMSG" ] || die "nothing to do"
+	fi
+	topmsg="$(cat "$git_dir/TG_EDITMSG")"
+	rm -f "$git_dir/TG_EDITMSG"
+fi
 
 
 ## Find starting commit to create the base
@@ -173,9 +255,14 @@ while [ -n "$merge" ]; do
 		info "It is also safe to abort this operation using:"
 		info "git$gitcdopt reset --hard some_branch"
 		info "(You are on a detached HEAD now.)"
-		echo "$name" >"$git_dir/top-name"
-		echo "$deps" >"$git_dir/top-deps"
-		echo "$merge" >"$git_dir/top-merge"
+		mkdir -p "$git_dir/tg-create"
+		printf '%s\n' "$name" >"$git_dir/tg-create/name"
+		printf '%s\n' "$deps" >"$git_dir/tg-create/deps"
+		printf '%s\n' "$merge" >"$git_dir/tg-create/merge"
+		printf '%s\n' "$msg" >"$git_dir/tg-create/msg"
+		printf '%s\n' "$topmsg" >"$git_dir/tg-create/topmsg"
+		printf '%s\n' "$nocommit" >"$git_dir/tg-create/nocommit"
+		printf '%s\n' "$noedit" >"$git_dir/tg-create/noedit"
 		exit 2
 	fi
 done
@@ -190,28 +277,36 @@ fi
 git update-ref "refs/top-bases/$name" "HEAD" ""
 git checkout -b "$name"
 
-echo "$deps" | sed 'y/ /\n/' >"$root_dir/.topdeps"
+printf '%s\n' $deps >"$root_dir/.topdeps"
 git add -f "$root_dir/.topdeps"
-
-author="$(git var GIT_AUTHOR_IDENT)"
-author_addr="${author%> *}>"
-{
-	echo "From: $author_addr"
-	! header="$(git config topgit.to)" || echo "To: $header"
-	! header="$(git config topgit.cc)" || echo "Cc: $header"
-	! header="$(git config topgit.bcc)" || echo "Bcc: $header"
-	! subject_prefix="$(git config topgit.subjectprefix)" || subject_prefix="$subject_prefix "
-	echo "Subject: [${subject_prefix}PATCH] $name"
-	echo
-	echo '<patch description>'
-	echo
-	[ "$(git config --bool format.signoff)" = true ] && echo "Signed-off-by: $author_addr"
-} >"$root_dir/.topmsg"
+printf '%s\n' "$topmsg" >"$root_dir/.topmsg"
 git add -f "$root_dir/.topmsg"
-echo "tg create $name" > "$git_dir/MERGE_MSG"
+printf '%s\n' "$msg" >"$git_dir/MERGE_MSG"
 
+if [ -n "$nocommit" ]; then
+	info "Topic branch $name set up."
+	if [ -n "$noedit" ]; then
+		info "Please fill in .topmsg now and make the initial commit."
+	else
+		info "Please make the initial commit."
+	fi
+	info "To abort:"
+	info "  git$gitcdopt rm -f .top* && git$gitcdopt checkout ${deps%% *} && $tgdisplay delete $name"
+	exit 0
+fi
 
-info "Topic branch $name set up. Please fill .topmsg now and make initial commit."
-info "To abort: git$gitcdopt rm -f .top* && git$gitcdopt checkout ${deps%% *} && $tgdisplay delete $name"
+git commit -m "$msg"
+subj="$(grep -i '^Subject:' <"$root_dir/.topmsg" | sed -n 1p || :)"
+subj="${subj#????????}"
+subj="${subj#*]}"
+subj="$(printf '%s\n' "$subj" | sed -e 's/^  *//; s/  *$//')"
+if [ -n "$subj" ]; then
+	printf '%s\n' "$subj" ""
+	sed -e '1,/^$/d' <"$root_dir/.topmsg"
+else
+	cat "$root_dir/.topmsg"
+fi >"$git_dir/MERGE_MSG"
+info "Topic branch $name created."
+exit 0
 
 # vim:noet
