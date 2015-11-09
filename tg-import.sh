d1e32289d78abf3a38ae3a3a3d3dd155ee0c67dc
Kyle J. McKay <mackyle@gmail.com>
2015-11-09 10:03:40 -0800

    tg-import.sh: overhaul to allow continuing and support new tg create
    When importing a range of commits and one of the imports prior to the
    last fails due to a conflict, it should now be possible to continue
    the rest of the import after fixing the conflict or deciding to skip
    the problem import (which could precipiate further conflicts).
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- tg-import.sh
+++ tg-import.sh
@@ -1,14 +1,18 @@
 #!/bin/sh
 # TopGit - A different patch queue manager
-# (c) Petr Baudis <pasky@suse.cz>  2008
-# (c) Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>  2008
+# Copyright (C) Petr Baudis <pasky@suse.cz>  2008
+# Copyright (C) Aneesh Kumar K.V <aneesh.kumar@linux.vnet.ibm.com>  2008
+# Copyright (C) Kyle J. McKay <mackyle@gmail.com>  2015
+# All rights reserved.
 # GPLv2
 
 branch_prefix=t/
 single=
 ranges=
+rangecnt=0
 basedep=
 
+USAGE="Usage: ${tgname:-tg} [...] import [-d <base-branch>] ([-p <prefix>] <range>... | -s <name> <commit>)"
 
 ## Parse options
 
@@ -22,10 +26,10 @@ while [ -n "$1" ]; do
 	-s)
 		single="$1"; shift;;
 	-*)
-		echo "Usage: ${tgname:-tg} [...] import [-d <base-branch>] ([-p <prefix>] <range>... | -s <name> <commit>)" >&2
+		printf '%s\n' "$USAGE" >&2
 		exit 1;;
 	*)
-		ranges="$ranges $arg";;
+		ranges="$ranges $arg"; rangecnt=$(( $rangecnt + 1 ));;
 	esac
 done
 
@@ -60,21 +64,34 @@ get_branch_name()
 	git log -1 --pretty=format:"%s" "$commit" | sed -e "$titleScript"
 }
 
+origbasedep="$basedep"
+isfirst=1
+lasthead=
+lastsymref=
+
 process_commit()
 {
 	commit="$1"
 	branch_name="$2"
 	info "---- Importing $commit to $branch_name"
-	$tg create "$branch_name" $basedep
+	lastsymref="$(git symbolic-ref --quiet HEAD || :)"
+	lasthead="$(git rev-parse --verify --quiet HEAD -- 2>/dev/null || :)"
+	$tg create --no-edit "$branch_name" $basedep
 	basedep=
 	get_commit_msg "$commit" > .topmsg
 	git add -f .topmsg .topdeps
+	if [ -n "$tgnosequester" ]; then
+		info "topgit.sequester is set to false, unadvisedly skipping sequester commit"
+	else
+		git commit -m "tg import create $branch_name"
+	fi
 	if ! git cherry-pick --no-commit "$commit"; then
 		info "The commit will also finish the import of this patch."
-		exit 2
+		return 2
 	fi
-	git commit -C "$commit"
+	git -c topgit.sequester=false commit -C "$commit"
 	info "++++ Importing $commit finished"
+	isfirst=
 }
 
 if [ -n "$single" ]; then
@@ -82,9 +99,59 @@ if [ -n "$single" ]; then
 	exit
 fi
 
+handle_pick_failure()
+{
+	# The import got stuck! Let the user fix it up.
+	info "You are in a subshell."
+	info "Please commit the cherry-pick resolution and then \`exit\`"
+	info "If you want to abort the cherry-pick,"
+	info "use \`exit 1\` to abort the tg import process at this point."
+	info "Use \`exit 2\` to skip importing this commit and continue."
+	if ! "${SHELL:-@SHELL_PATH@}" -i </dev/tty; then
+		ret=$?
+		if [ $ret -eq 2 ]; then
+			info "Ok, I will try to continue without importing this commit."
+			if [ -n "$tgnosequester" ]; then
+				git reset --hard HEAD
+			else
+				git reset --hard HEAD^
+			fi
+			[ -z "$isfirst" ] || basedep="$(origbasedep)"
+			[ -z "$lasthead" ] || git update-ref --no-deref HEAD "$lasthead"
+			[ -z "$lastsymref" ] || git symbolic-ref HEAD "$lastsymref"
+			git update-ref -d "refs/top-bases/$branch_name" || :
+			git update-ref -d "refs/heads/$branch_name" || :
+			git reset --hard HEAD
+			return 0
+		else
+			info "Ok, you aborted the import operation at this point.  Now, you just need"
+			info "to switch back to some sane branch using \`git$gitcdopt checkout\`."
+			exit 3
+		fi
+	fi
+}
+
+handle_one_commit()
+{
+	case "$sign" in
+		'-')
+			info "Merged already: $comment"
+			;;
+		*)
+			if ! process_commit "$rev" "$branch_prefix$(get_branch_name "$rev")"; then
+				ret=$?
+				[ -z "$islast" ] || return $ret
+				handle_pick_failure
+			fi
+			;;
+	esac
+}
+
 # nice arg verification stolen from git-format-patch.sh
-for revpair in $ranges
-do
+rangeidx=0
+islast=
+for revpair in $ranges; do
+	rangeidx=$(( $rangeidx + 1 ))
 	case "$revpair" in
 	?*..?*)
 		rev1=`expr "z$revpair" : 'z\(.*\)\.\.'`
@@ -98,18 +165,18 @@ do
 		die "Not a valid rev $rev1 ($revpair)"
 	git rev-parse --verify "$rev2^0" -- >/dev/null 2>&1 ||
 		die "Not a valid rev $rev2 ($revpair)"
-	git cherry -v "$rev1" "$rev2" |
-	while read sign rev comment
-	do
-		case "$sign" in
-		'-')
-			info "Merged already: $comment"
-			;;
-		*)
-			process_commit "$rev" "$branch_prefix$(get_branch_name "$rev")"
-			;;
-		esac
-	done
+	git cherry -v "$rev1" "$rev2" | {
+		if read sign rev comment; then
+			while read next_sign next_rev next_comment; do
+				handle_one_commit
+				sign="$next_sign"
+				rev="$next_rev"
+				comment="$next_comment"
+			done
+			[ "$rangeidx" != "$rangecnt" ] || islast=1
+			handle_one_commit
+		fi
+	}
 done
 
 # vim:noet
