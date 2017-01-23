20439b1593689ceb6bac22072dbd3053fae3a4ac
Kyle J. McKay <mackyle@gmail.com>
2017-01-22 20:36:40 -0800

    tg-annihilate.sh: suppress `tg update` with --no-update
    After annihilation is complete `tg update` is run by default
    on any modified dependents.  Giving `--no-update`` suppresses
    this action.
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- tg-annihilate.sh
+++ tg-annihilate.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 # TopGit - A different patch queue manager
-# (c) Petr Baudis <pasky@suse.cz>  2008
-# (c) Per Cederqvist <ceder@lysator.liu.se>  2010
+# (C) Petr Baudis <pasky@suse.cz>  2008
+# (C) Per Cederqvist <ceder@lysator.liu.se>  2010
+# (C) Kyle J. McKay <mackyle@gmail.com>  2017
+# All rights reserved.
 # GPLv2
 
 force= # Whether to annihilate non-empty branch, or branch where only the base is left.
+update=1 # Whether to run tg update on affected branches
 
 
 ## Parse options
@@ -14,8 +17,12 @@ while [ -n "$1" ]; do
 	case "$arg" in
 	-f|--force)
 		force=1;;
+	--no-update)
+		update=;;
+	--update)
+		update=1;;
 	*)
-		echo "Usage: ${tgname:-tg} [...] annihilate [-f]" >&2
+		echo "Usage: ${tgname:-tg} [...] annihilate [-f] [--no-update]" >&2
 		exit 1;;
 	esac
 done
@@ -52,19 +59,26 @@ $($tg next)
 EOT
 
 info "branch successfully annihilated: $name"
+now="now"
 if [ -n "$updatelist" ]; then
-	info "now updating affected branches: $updatelist"
-	while read dependent && [ -n "$dependent" ]; do
-		$tg update "$dependent"
-	done <<-EOT
-	$(sed 'y/ /\n/' <<-LIST
-	$updatelist
-	LIST
-	)
-	EOT
+	if [ -n "$update" ]; then
+		info "now updating affected branches: $updatelist"
+		while read dependent && [ -n "$dependent" ]; do
+			$tg update "$dependent"
+		done <<-EOT
+		$(sed 'y/ /\n/' <<-LIST
+		$updatelist
+		LIST
+		)
+		EOT
+	else
+		info "skipping update because --no-update given"
+		info "be sure to update affected branches: $updatelist"
+		now="after updating"
+	fi
 fi
 
-info "If you have shared your work, you might want to run ${tgname:-tg} push $name now."
+info "If you have shared your work, you might want to run ${tgname:-tg} push $name $now."
 git status
 
 # vim:noet
