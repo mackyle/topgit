1389e3266b094c039179a6f78d99e02033b9dda2
Kyle J. McKay <mackyle@gmail.com>
2017-02-15 03:58:26 -0800

    tg-base.sh: understand --short[=n] and --no-short options
    Pass the standard --short, --short=n, --no-short options on
    through to git rev-parse.
    
    Also complain about unrecognized options, allow '--' to separate
    options from branch names and make sure '@' is recognized as 'HEAD'.
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- tg-base.sh
+++ tg-base.sh
@@ -1,18 +1,40 @@
 #!/bin/sh
 # TopGit - A different patch queue manager
-# (c) Petr Baudis <pasky@suse.cz>  2008
-# (c) Per Cederqvist <ceder@lysator.liu.se>  2010
+# (C) Petr Baudis <pasky@suse.cz>  2008
+# (C) Per Cederqvist <ceder@lysator.liu.se>  2010
+# (C) Kyle J. McKay <mackyle@gmail.com>  2017
+# All rights reserved.
 # GPLv2
+
+USAGE="Usage: ${tgname:-tg} [...] base [--short[=n] | --no-short] [--] [branch...]"
+
+usage()
+{
+	if [ "${1:-0}" != 0 ]; then
+		printf '%s\n' "$USAGE" >&2
+	else
+		printf '%s\n' "$USAGE"
+	fi
+	exit ${1:-0}
+}
 
 ## Parse options
 
-case "$1" in
+short="--short"
+
+while [ $# -gt 0 ]; do case "$1" in
 	-h|--help)
-		echo "Usage: ${tgname:-tg} [...] base [ branch... ]" >&2
-		exit 0;;
+		usage;;
+	--short|--short=*|--no-short)
+		short="$1";;
+	--)
+		shift
+		break;;
+	-?*)
+		die "unrecognized option: $1";;
 	*)
 		break;;
-esac
+esac; shift; done
 
 if [ "$#" -eq 0 ]; then
 	set -- HEAD
@@ -20,8 +42,9 @@ fi
 
 rv=0
 for rev in "$@"; do
+	[ "$rev" != "@" ] || rev="HEAD"
 	name="$(strip_ref "$(git symbolic-ref -q "$rev" 2>/dev/null || echol "$rev")")"
-	git rev-parse --short --verify "refs/$topbases/$name" -- 2>/dev/null || {
+	git rev-parse --verify $short "refs/$topbases/$name" -- 2>/dev/null || {
 		rv=1
 		echo "$rev is not a TopGit branch" >&2
 	}
