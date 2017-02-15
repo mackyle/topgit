56d71ffb739fec57ca4b4ed40e3f33a7e954fd4d
Kyle J. McKay <mackyle@gmail.com>
2017-02-15 12:38:36 -0800

    t1030: test tg -C option used multiple times
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

---
+++ t/t1030-tg-cding-around.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+test_description='tg -C used multiple times works properly'
+
+TEST_NO_CREATE_REPO=1
+
+. ./test-lib.sh
+
+GIT_CEILING_DIRECTORIES="$PWD" && export GIT_CEILING_DIRECTORIES
+
+test_plan 3
+
+test_expect_success 'test setup' '
+	test_create_repo refs &&
+	test_create_repo heads &&
+	git -C refs config topgit.top-bases refs &&
+	git -C heads config topgit.top-bases heads &&
+	test "$(cd refs && git config topgit.top-bases)" = "refs" &&
+	test "$(cd heads && git config topgit.top-bases)" = "heads" &&
+	mkdir sub &&
+	mkdir sub/dir
+'
+
+match_str() {
+	test "$1" = "$2"
+}
+
+test_expect_success 'tg -C gets to refs' '
+	match_str "refs/top-bases" "$(tg -C refs --top-bases)" &&
+	cd sub &&
+	match_str "refs/top-bases" "$(tg -C dir -C .. -C ../refs --top-bases)"
+'
+
+test_expect_success 'tg -C gets to heads' '
+	match_str "refs/heads/{top-bases}" "$(tg -C heads --top-bases)" &&
+	cd sub &&
+	match_str "refs/heads/{top-bases}" "$(tg -C dir -C .. -C ../heads --top-bases)"
+'
+
+test_done
