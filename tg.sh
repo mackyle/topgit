2b69bdcd3f974de58756cef4daeeea3ea3027374
Kyle J. McKay <mackyle@gmail.com>
2017-03-29 11:53:04 -0700

    tg.sh: preload .ref cache
    After all the ref cache dirs have been created, preload all
    the known .ref values all at once rather than painfully one
    at a time later as each ref is read for the first time.
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- tg.sh
+++ tg.sh
@@ -524,6 +524,13 @@ create_ref_dirs()
 {
 	[ ! -s "$tg_tmp_dir/tg~ref-dirs-created" -a -s "$tg_ref_cache" ] || return 0
 	awk -v p="$tg_tmp_dir/cached/" '{print p $1}' <"$tg_ref_cache" | tr '\n' '\0' | xargs -0 mkdir -p
+	awk -v p="$tg_tmp_dir/cached/" '
+		NF == 2 && $1 ~ /^refs\/./ && $2 ~ /^[0-9a-fA-F]{4,}$/ {
+			fn = p $1 "/.ref"
+			print "0 " $2 >fn
+			close fn
+		}
+	' <"$tg_ref_cache"
 	echo 1 >"$tg_tmp_dir/tg~ref-dirs-created"
 }
 
