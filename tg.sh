51b0bd5c3bedb6b0aaf22616e7a190af6b9aed6f
Kyle J. McKay <mackyle@gmail.com>
2017-08-30 18:39:00 -0700

    topgit: version 0.19.9
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- tg.sh
+++ tg.sh
@@ -5,7 +5,7 @@
 # All rights reserved.
 # GPLv2
 
-TG_VERSION="0.19.9.PRE"
+TG_VERSION="0.19.9"
 
 # Update in Makefile if you add any code that requires a newer version of git
 GIT_MINIMUM_VERSION="@mingitver@"
