33333cf7ecaf2b9b07ff7eca930b259fbb3be6c8
Kyle J. McKay <mackyle@gmail.com>
2021-08-21 05:14:11 -0700

    t8300: compensate for Git sha256 patch-id behavior
    The help for `git patch-id` says:
    
      Read a patch from the standard input and compute the patch ID for it.
    
      A "patch ID" is nothing but a sum of SHA-1 of the file diffs associated
      [...]
    
    It neglects to mention anything about SHA2 sha256 or anything about the
    output being affected by whether or not the current directory is a valid
    `git rev-parse --git-dir` or not.
    
    And yet, if the current directory is a valid `git rev-parse --git-dir`
    directory AND it happens to be using sha256 instead of sha1, then the
    output of `git patch-id` will be using sha256 instead of sha1.
    
    There's, of course, nothing about this in the help for `git patch-id`
    at all -- it continues to reference SHA-1.
    
    However, simply setting the git directory (using GIT_DIR, --git-dir or -C),
    addresses the problem.
    
    A very simple fix for a non-obvious issue which is, fortunately,
    future-proof if the `git patch-id` behavior ends up getting changed.
    
    Signed-off-by: Kyle J. McKay <mackyle@gmail.com>

--- t/t8300-wayback-export.sh
+++ t/t8300-wayback-export.sh
@@ -42,13 +42,13 @@ test_expect_success 'setup' '
 
 test_expect_success 'patch first' '
 	tg -C patches -w t/first patch patch >p1 &&
-	git patch-id <p1 >p1.id &&
+	git -C patches patch-id <p1 >p1.id &&
 	test_diff patch1.id p1.id
 '
 
 test_expect_success 'patch second' '
 	tg -C patches -w : patch patch >p2 &&
-	git patch-id <p2 >p2.id &&
+	git -C patches patch-id <p2 >p2.id &&
 	test_diff patch2.id p2.id
 '
 
@@ -61,7 +61,7 @@ test_expect_success 'tg export --linearize bare no go' '
 test_expect_success 'tg export --collapse bare ok' '
 	tg -C patches.git export --collapse out &&
 	git -C patches.git diff-tree -p out^! >co.patch &&
-	git patch-id <co.patch >co.id &&
+	git -C patches.git patch-id <co.patch >co.id &&
 	test_diff co.id patch2.id &&
 	git -C patches.git update-ref -d refs/heads/out out
 '
@@ -69,7 +69,7 @@ test_expect_success 'tg export --collapse bare ok' '
 test_expect_success 'tg export --quilt bare ok' '
 	mkdir qu &&
 	tg -C patches.git export --quilt "$PWD/qu" &&
-	git patch-id <qu/patch.diff >qu.id &&
+	git -C patches.git patch-id <qu/patch.diff >qu.id &&
 	test_diff qu.id patch2.id
 '
 
@@ -78,7 +78,7 @@ test_expect_success 'tg export --quilt bare ok' '
 test_expect_success 'tg export --collapse wayback first' '
 	tg -C patches.git -w t/first export --collapse out &&
 	git -C patches.git diff-tree -p out^! >wc1.patch &&
-	git patch-id <wc1.patch >wc1.id &&
+	git -C patches.git patch-id <wc1.patch >wc1.id &&
 	test_diff wc1.id patch1.id &&
 	git -C patches.git update-ref -d refs/heads/out out
 '
@@ -86,7 +86,7 @@ test_expect_success 'tg export --collapse wayback first' '
 test_expect_success 'tg export --collapse wayback second' '
 	tg -C patches.git -w : export --collapse out &&
 	git -C patches.git diff-tree -p out^! >wc2.patch &&
-	git patch-id <wc2.patch >wc2.id &&
+	git -C patches.git patch-id <wc2.patch >wc2.id &&
 	test_diff wc2.id patch2.id &&
 	git -C patches.git update-ref -d refs/heads/out out
 '
@@ -94,7 +94,7 @@ test_expect_success 'tg export --collapse wayback second' '
 test_expect_success 'tg export --linearize wayback first' '
 	tg -C patches.git -w t/first export --linearize out &&
 	git -C patches.git diff-tree -p out^! >wl1.patch &&
-	git patch-id <wl1.patch >wl1.id &&
+	git -C patches.git patch-id <wl1.patch >wl1.id &&
 	test_diff wl1.id patch1.id &&
 	git -C patches.git update-ref -d refs/heads/out out
 '
@@ -102,20 +102,20 @@ test_expect_success 'tg export --linearize wayback first' '
 test_expect_success 'tg export --linearize wayback second' '
 	tg -C patches.git -w : export --linearize out &&
 	git -C patches.git diff-tree -p out^! >wl2.patch &&
-	git patch-id <wl2.patch >wl2.id &&
+	git -C patches.git patch-id <wl2.patch >wl2.id &&
 	test_diff wl2.id patch2.id &&
 	git -C patches.git update-ref -d refs/heads/out out
 '
 
 test_expect_success 'tg export --quilt wayback first' '
 	tg -C patches.git -w t/first export --quilt "$PWD/qu1" &&
-	git patch-id <qu1/patch.diff >qu1.id &&
+	git -C patches.git patch-id <qu1/patch.diff >qu1.id &&
 	test_diff qu1.id patch1.id
 '
 
 test_expect_success 'tg export --quilt wayback second' '
 	tg -C patches.git -w : export --quilt "$PWD/qu2" &&
-	git patch-id <qu2/patch.diff >qu2.id &&
+	git -C patches.git patch-id <qu2/patch.diff >qu2.id &&
 	test_diff qu2.id patch2.id
 '
 
